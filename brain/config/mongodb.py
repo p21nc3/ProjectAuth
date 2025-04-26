b01d2a031d40c4716c63d52c4dcb119c11b88e1a
p21nc3 <pp4768@srmist.edu.in>
2025-04-26 16:46:37 +0100

    worker+mongo

--- brain/config/mongodb.py
+++ brain/config/mongodb.py
@@ -2,9 +2,15 @@ from flask_pymongo import PyMongo
 
 
 def config_mongodb(app):
-    mongodb_host = app.config["MONGODB_HOST"]
-    mongodb_port = app.config["MONGODB_PORT"]
-    app.config["MONGO_URI"] = f"mongodb://{mongodb_host}:{mongodb_port}/ProjectAuth"
+    mongodb_host = app.config.get('MONGODB_HOST', 'mongodb')
+    mongodb_port = app.config.get('MONGODB_PORT', 27017)
+    mongodb_username = app.config.get('MONGODB_USERNAME', 'admin')
+    mongodb_password = app.config.get('MONGODB_PASSWORD', 'changeme')
+    mongodb_database = app.config.get('MONGODB_DATABASE', 'sso-monitor')
+    mongodb_auth_source = app.config.get('MONGODB_AUTH_SOURCE', 'admin')
+    
+    # Construct URI with authentication
+    app.config['MONGO_URI'] = f'mongodb://{mongodb_username}:{mongodb_password}@{mongodb_host}:{mongodb_port}/{mongodb_database}?authSource={mongodb_auth_source}'
     mongo = PyMongo(app)
-    app.extensions["db"] = mongo.db
-    app.config["db"] = mongo.db # deprecated
+    app.extensions['db'] = mongo.db
+    app.config['db'] = mongo.db  # deprecated 
